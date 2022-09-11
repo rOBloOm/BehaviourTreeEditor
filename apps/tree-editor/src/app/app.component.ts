f31898a8410d9bdeff9be897c3b389f50b5ecfe3
Roland Hof <rhof@techgroup.ch>
2022-09-11 23:13:45 +0200

    Initial commit

---
+++ apps/tree-editor/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sweet-potato-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'tree-editor';
+}
