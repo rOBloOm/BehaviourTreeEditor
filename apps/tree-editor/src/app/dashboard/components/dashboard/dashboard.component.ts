0b0545149f49dcf19e9998d5afa0d6f438c99c11
rOBloOm <roland.hof@gmail.com>
2023-04-12 10:52:34 +0200

    removed fxflex from dashboard

--- apps/tree-editor/src/app/dashboard/components/dashboard/dashboard.component.ts
+++ apps/tree-editor/src/app/dashboard/components/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { FlexModule } from '@angular/flex-layout';
 import { RouterOutlet } from '@angular/router';
 import { LeafNodeImportService } from '../../services/leaf-node-import.service';
 import { DashboardMenuComponent } from '../dashboard-menu/dashboard-menu.component';
@@ -9,7 +8,7 @@ import { DashboardMenuComponent } from '../dashboard-menu/dashboard-menu.compone
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
-  imports: [DashboardMenuComponent, RouterOutlet, FlexModule],
+  imports: [DashboardMenuComponent, RouterOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [LeafNodeImportService],
 })
